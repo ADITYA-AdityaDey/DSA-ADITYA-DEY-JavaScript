e13ab77187f5e8612a63c29f86a9931b9eda5795
Aditya Dey <deyaditya2004@gmail.com>
2024-06-17 17:19:40 +0530

    add more in Time & Space Complexit Directory

--- Time & Space Complexity/Notes2_Time_&_Space_Complexity.js
+++ Time & Space Complexity/Notes2_Time_&_Space_Complexity.js
@@ -41,3 +41,76 @@ Example 2:
 
 
 */
+
+
+
+// ***** Nested Loops ------->  1. Dependency [Trace & Find]  2. No Dependency [Multiply Time Complexity  of each block]
+
+
+
+/* 
+Example 3:
+
+    for (let i = n, let j = 0; i <= i / 2; j = j + 1)
+    {
+        ----
+        ----
+    }
+
+//so here, i = n           n > 0            j = 0
+           i = n / 2       n / 2 > 0        j = 1
+           i = n / 4       n / 4 > 0        j = 2
+           i = n / 8       n / 8 > 0        j = 4
+           
+           so, eerytime n value reduced by 1/2
+            n / 2*k >= 0
+            n = 2*k
+            k = log2n (log base2 n)
+
+            so, Time Complexity => O(log2n)
+
+*/
+
+
+
+// *** Always focuson Conditions for example (i > 0) not others like j=j+1  (----> this is from Example 3)
+// because conditions (i > 0) is the reason for itterartion
+
+
+
+/*
+Example 4:
+
+    for (let i = 0; let j = n; j > 0; i = i / 2; j = j + 1){
+        ------
+        ------
+    }
+
+
+// so here, i=0             n>0         j=n
+            i=i/2           n+1>0       j=n+1
+            i=i/4           n+2>0       j=n+2
+                            n+3>0       j=n+3
+                            it's go for infinite times
+                            so it's a Infine Loop
+
+
+                    ----> Time Complexity (of Infinite Loop is always Undetermined) => Undetermined
+
+*/
+
+
+
+/*
+Example 5:
+
+    for (let i = 1; i <= n; i = i *  2){     -------------------> Time Complexity = log2n
+        for (let j = 1; j <= n; j = j + 2){  --------------------> Time Complexity = n (from n / 2 remove the constants 2 so it's become n)
+            --------
+            --------
+        }
+    }
+
+            // Time Complexity = O(nlog2n) [Multiplication for indepedent itteration]
+
+*/
